b4857e0184cab2fe3bef9b1a687b32692ac04b14
Isaac Eromosele Esezobor <eieromosele@gmail.com>
2022-10-25 12:48:34 -0700

    commited

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - wites a function that takes a pointer to
+ * an int as parameter and updates the v
+ * alue it points to to 98
+ * @n: Pointer to the variable to update
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
